74a1d68bd5aa7fbbdb5c6449798cee9f05145a92
pellju <juho.64@hotmail.com>
2024-05-19 20:15:40 +0300

    Adding TypeScript-files and created a simple web-server

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world!');
+});
+
+app.listen(3000, () => {
+    console.log("Server is running at http://127.0.0.1:3000 !");
+});
