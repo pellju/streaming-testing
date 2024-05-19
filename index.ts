6619e9ebdf4a4c02a0fcbcbb61f5bbf41faa9a3f
pellju <juho.64@hotmail.com>
2024-05-19 21:46:03 +0300

    Creating an ffmpeg-stream works, cuts in 10 seconds

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import express, { Express, Request, Response } from 'express';
-import ffmpeg from 'fluent-ffmpeg';
+import ffmpeg, {FfmpegCommand} from 'fluent-ffmpeg';
 
 const app: Express = express();
 app.use(express.json());
+app.use(express.static(__dirname + '/streams'));
 
 var input: string = 'test';
 
@@ -17,7 +18,25 @@ app.post('/test', (req: Request, res: Response) => {
         res.json({ 'Error': 'Incorrect body'});
     } else {
         input = body.input;
-        res.json({ 'Info': 'Success' });
+        try {
+            ffmpeg(input)
+            .addOption('-c', 'copy')
+            .addOption('-f', 'hls')
+            .addOption('-hls_time', '10')
+            .addOption('-hls_list_size', '3')
+            .addOption('-hls_delete_threshold', '3')
+            .addOption('-hls_flags delete_segments')
+            .on('error', function(err) {
+                console.log('Following error:');
+                console.log(err.message);
+            })
+            .save(__dirname + '/streams/testing.m3u8');
+            res.send({ 'Information': "Test works!"});
+        } catch(e: any) {
+            console.log('Error!');
+            console.log(e.message);
+            res.send({'Error': 'Shit hit the fan'});
+        }
     }
     
 });
@@ -26,7 +45,7 @@ app.get('/test', (req: Request, res: Response) => {
     if (input === 'test') {
         res.status(400).json({ 'Error': 'Please change the input first!' });
     } else {
-        res.send({ 'Information': "Test works!"});
+        res.send({ 'Information': "Success"});
     }
 })
 
