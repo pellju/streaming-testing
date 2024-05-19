a6f1413410f0122353fe1b08b05a20b9d6d0624b
pellju <juho.64@hotmail.com>
2024-05-19 20:48:33 +0300

    Creating additional router for POST-request

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
 import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello world!');
+});
+
+app.post('/test', (req: Request, res: Response) => {
+    const body = req.body;
+
+    if (body === undefined || body.input === undefined) {
+        res.json({ 'Error': 'Incorrect body'});
+    } else {
+        res.json({ 'Info': 'Success' });
+    }
+    
 });
 
 app.listen(3000, () => {
