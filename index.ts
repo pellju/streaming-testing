4168fbf2078bdf22222d6597af21c5480a20d2a2
pellju <juho.64@hotmail.com>
2024-05-25 23:02:21 +0300

    Adding a route file for stream-management -related routes

--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import path from 'path';
 
 import { apiCheckerMiddleware } from './middlewares/streamingMiddleware';
 import { addNewApiKey, deleteApiKey, listKeys } from './controllers/apiController';
-import { addStream, listStreamNames, removeStream } from './controllers/streamController';
+import { streamingRouter } from './routes/streamingRoutes';
+//import { addStream, listStreamNames, removeStream } from './controllers/streamController';
 
 // Creating a new Express-server and allowing /stream-paths to access streams-folder (statically)
 const app: Express = express();
@@ -17,16 +18,11 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello world!');
 });
 
-app.post('/newstream', addStream);
-
-app.get('/streams', listStreamNames);
-
-app.delete('/remove/:name', removeStream);
+app.use(streamingRouter);
 
 app.post('/newapikey', addNewApiKey);
 
 app.delete('/apikeyremoval/:apikey', deleteApiKey);
-
 
 app.get('/keys', listKeys);
 
