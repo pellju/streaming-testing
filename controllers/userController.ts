b0522278a5e64a66d73356cb3188d4ac4f1a00c2
pellju <juho.64@hotmail.com>
2024-05-26 18:32:57 +0300

    Adding checks for MongoDB-related checks to registration and login

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,4 +1,5 @@
 import {Request, Response } from 'express';
+import { db } from "../models";
 
 const userRegistration = (req: Request, res: Response) => {
     const body = req.body;
@@ -13,20 +14,60 @@ const userRegistration = (req: Request, res: Response) => {
     } else if (password === undefined) {
         return res.status(400).json({ 'Error': 'Missing password!'});
     } else if (invitecode === undefined) {
-        return res.status(400).json({ 'Error': 'Missing invite!'});
+        return res.status(400).json({ 'Error': 'Missing invitecode!'});
     } else {
         try {
+
+            // Set types better:
+            db.User.findOne({username: username}).exec((err: any, user: any) => {
+                if (err) {
+                    res.status(500).json({ 'Error': 'Internal error related to MongoDB!'});
+                } else if (user) {
+                    res.status(400).json({ 'Error': 'User with given username exists!' });
+                }
+            });
+
+            // Add registration function here
 
         } catch (e: any) {
             console.log("Registration error!");
             console.log(e.message);
-            res.status(400).json({ 'Error': 'Unknown error!'});
+            res.status(500).json({ 'Error': 'Unknown error!'});
         }
     }
 }
 
 const userLogin = (req: Request, res: Response) => {
+    const body = req.body;
+    const username: string = body.username;
+    const password: string = body.password;
 
+    if (body === undefined) {
+        return res.status(400).json({ 'Error': 'Login registration data!'});
+    } else if (username === undefined) {
+        return res.status(400).json({ 'Error': 'Missing username!'});
+    } else if (password === undefined) {
+        return res.status(400).json({ 'Error': 'Missing password!'});
+    } else {
+        try {
+
+            // Set types better:
+            db.User.findOne({username: username}).exec((err: any, user: any) => {
+                if (err) {
+                    res.status(500).json({ 'Error': 'Internal error related to MongoDB!'});
+                } else if (user) {
+                    // Adding login service here!
+                } else {
+                    res.status(400).json({ 'Error': 'Username not found' });
+                }
+            });
+
+        } catch (e: any) {
+            console.log("Registration error!");
+            console.log(e.message);
+            res.status(500).json({ 'Error': 'Unknown error!'});
+        }
+    }
 }
 
 export { userRegistration, userLogin }
--- models/createRoles.ts
+++ models/createRoles.ts
@@ -67,5 +67,3 @@ const createRoles = () => {
         }
     })
 }
-
-export { createRoles }
