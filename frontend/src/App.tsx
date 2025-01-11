22308fda8be8da9102fc5d38fe3649f9531ec4a9
pellju <juho.64@hotmail.com>
2025-01-11 20:51:12 +0200

    Adding a simple registration page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+// Importing Registration form
+import Registration from './components/Registration'
+
+type UsernameProps = {
+  value: string,
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+type PasswordProps = {
+  value: string,
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+type InviteProps = {
+  value: string,
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+}
 
 function App() {
   //const [count, setCount] = useState(0);
@@ -9,6 +27,28 @@ function App() {
   const [showLogin, setShowLogin] = useState<boolean>(false);
   const [showRegister, setShowRegister] = useState<boolean>(false);
 
+  // Adding registration values:
+  const [regUsername, setRegUsername] = useState<string>('');
+  const [regPassword, setRegPassword] = useState<string>('');
+  const [confRegPassword, setConfRegPassword] = useState<string>('');
+  const [regInvite, setRegInvite] = useState<string>('');
+  // regUsername, setRegUsername, regPassword, setRegPassword, confRegPassword, setConfRegPassword, regInvite, setRegInvite
+
+  // Adding login values:
+  const [loginUsername, setLoginUsername] = useState<string>('');
+  const [loginPassword, setLoginPassword] = useState<string>('');
+
+
+  // Handling registration:
+  const handlingRegistration = async (event: any) => {
+    event.preventDefault();
+
+    console.log("handlingRegistration...");
+    setRegUsername('')
+    setRegPassword('')
+    setConfRegPassword('')
+    setRegInvite('')
+  }
   /*return (
     <>
       <div>
@@ -33,11 +73,21 @@ function App() {
       </p>
     </>
   )*/
- return (
-  <div>
-    <center>Hello world!</center>
-  </div>
- )
+ if (loggedIn) {
+  return (
+    <div>
+      <center>You have logged in successfully!</center>
+    </div>
+   )
+ } else {
+  return (
+    <div>
+      <h2><center>Hello world, and welcome!</center></h2>
+      <center>Registyration:</center>
+      <Registration regUsername={regUsername} setRegUsername={setRegUsername} regPassword={regPassword} setRegPassword={setRegPassword} confRegPassword={confRegPassword} setConfRegPassword={setConfRegPassword} regInvite={regInvite} setRegInvite={setRegInvite} handleRegistration={handlingRegistration} />
+    </div>
+   )
+ }
 }
 
 export default App
---
+++ frontend/src/components/Registration.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useState } from 'react'
+
+interface RegistrationProps {
+    regUsername: string,
+    setRegUsername: any, // ToDo: Find a proper type
+    regPassword: string,
+    setRegPassword: any, // ToDo: Find a proper type
+    confRegPassword: string,
+    setConfRegPassword: any, // ToDo: Find a proper type
+    regInvite: string,
+    setRegInvite: any, // ToDo: Find a proper type
+    handleRegistration: any // ToDo: Find a proper type
+}
+
+const Registration: React.FC<RegistrationProps> = ({ regUsername, setRegUsername, regPassword, setRegPassword, confRegPassword, setConfRegPassword, regInvite, setRegInvite, handleRegistration }) => {
+
+    return (
+        <div id="registrationForm">
+            <form onSubmit={handleRegistration}>
+                    <div>
+                        Username: <input type='text' value={regUsername} name='regUsername' onChange={({target}) => setRegUsername(target.value)}/>
+                    </div>
+                    <div>
+                        Password: <input type='password' value={regPassword} name='regPassword' onChange={({target}) => setRegPassword(target.value)}/>
+                    </div>
+                    <div>
+                        Confirm Password: <input type='password' value={confRegPassword} name='regPassword' onChange={({target}) => setConfRegPassword(target.value)}/>
+                    </div>
+                    <div>
+                        Invitecode: <input type='text' value={regInvite} name='regPassword' onChange={({target}) => setRegInvite(target.value)}/>
+                    </div>
+                    <button id='registration-button' type='submit'>Register</button>
+            </form>
+        </div>
+    )
+}
+
+export default Registration;
