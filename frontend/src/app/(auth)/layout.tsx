c4bc7a4608801153636767d181eef13f85a8ceb6
pellju <juho.64@hotmail.com>
2025-01-11 16:03:22 +0200

    Removing existing frontend and replacing it with React

--- frontend/src/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function AuthLayout({ children }: {
-    readonly children: React.ReactNode;
-}) {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            {children}
-        </div>
-    );
-}
